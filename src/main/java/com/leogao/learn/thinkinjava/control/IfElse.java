e8445c06780d354b898175d82f0ca8a38f3af151
LeoGSH <littlegao1989@126.com>
2018-05-10 19:57:46 +0800

    merge测试2

--- src/main/java/com/leogao/learn/thinkinjava/control/IfElse.java
+++ src/main/java/com/leogao/learn/thinkinjava/control/IfElse.java
@@ -1,25 +1,27 @@
+package com.leogao.learn.thinkinjava.control;
 
+import static com.leogao.learn.thinkinjava.net.mindview.util.Print.print;
 
 public class IfElse {
-  static int result = 0;
-  static void test(int testval, int target) {
-    if(testval > target)
-      result = +1;
-    else if(testval < target)
-      result = -1;
-    else
-      result = 0; // Match
-  }
-  public static void main(String[] args) {
-    test(10, 5);
-    print(result);
-    test(5, 10);
-    print(result);
-    test(5, 5);
-    print(result);
-  }
-} /* Output:
-1
--1
-0
-*///:~
+    static int result = 0;
+
+    static void test(int testval, int target) {
+        if (testval > target)
+            result = +1;
+        else if (testval < target)
+            result = -1;
+        else
+            result = 0; // Match
+    }
+
+    public static void main(String[] args) {
+        test(10, 5);
+        print(result);
+        test(5, 10);
+        print(result);
+        test(5, 5);
+        print(result);
+    }
+} /*
+   * Output: 1 -1 0
+   *///:~
